67a503af13d92bd65ff5ad075b3124f0f0997899
YASH JAIN <2910jainyash@gmail.com>
2024-11-23 16:01:19 +0530

    Added optional operator

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -4,6 +4,8 @@ import NewsItem from "./NewsItem"
 const NewsBoard = ({category}) => {
 
   const [articles, setarticles]= useState ([]);
+  console.log("ARTICLES....", articles);
+  
 
   useEffect(()=> {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
@@ -15,7 +17,7 @@ const NewsBoard = ({category}) => {
   return (
     <div>
       <h2 className='text-center'> latest <span className="badge bg-danger">News</span></h2>
-      {articles.map((news,index)=>{
+      {articles?.map((news,index)=>{
         return <NewsItem key={index} title= {news.title} description={news.description} src={news.urlToImage} url={news.url}/>
       })}
     </div>
