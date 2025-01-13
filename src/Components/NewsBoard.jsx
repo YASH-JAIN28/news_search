4fe8e437d9be8be836d5916ab14529ff579ddb6f
YASH JAIN <2910jainyash@gmail.com>
2025-01-13 13:01:02 +0530

    changes

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,29 +1,38 @@
-import { useEffect, useState } from "react"
-import NewsItem from "./NewsItem"
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
 
-const NewsBoard = ({category}) => {
-
-  const [articles, setarticles]= useState ([]);
+const NewsBoard = ({ category }) => {
+  const [articles, setarticles] = useState([]);
   console.log("ARTICLES....", articles);
-  
 
-  useEffect(()=> {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
     fetch(url)
-    .then(response => response.json())
-    .then(data => setarticles(data.articles));
-    console.log("abc",import.meta.env.VITE_API_KEY);
-    
-
-  },[category])
+      .then((response) => response.json())
+      .then((data) => setarticles(data.articles));
+    console.log("abc", import.meta.env.VITE_API_KEY);
+  }, [category]);
   return (
     <div>
-      <h2 className='text-center'> latest <span className="badge bg-danger">News</span></h2>
-      {articles?.map((news,index)=>{
-        return <NewsItem key={index} title= {news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+      <h2 className="text-center">
+        {" "}
+        latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles?.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 export default NewsBoard;
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -2,14 +2,14 @@ import image from '../assets/news.jpeg'
 
 const NewsItem = ({ title, description, src,url }) => {
   return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
+    <div className="card bg-dark text-light mb-4 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
   <img src={src?src:image} style={{height: "210px",width:"320px"}} className="card-img-top" alt="News"/>
   <div className="card-body">
     <h5 className="card-title">{title?title.slice(0,50):"no title available"} </h5>
     <p className="card-text">{ description?description.slice(0,90):"no description available"}</p>
     <a href={url} className="btn btn-primary">Read More</a>
   </div>
-</div>
+  </div>
   )
 }
 
